76b2a04adf9fca2478b817a13289df3691d8df7b
wishoon <heonga2@gmail.com>
2021-02-05 03:27:19 +0900

    일자리 검색, 일자리 신청, 일자리 취소 분기 처리 추가

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, Blueprint
 from flask_restplus import Resource, Api
 # API Import
 from apis.member import *
+from apis.search import Search, Category
+from apis.item import Work, Apply, Cancel
 
 app = Flask(__name__)
 
@@ -10,13 +12,17 @@ api = Api(blueprint, version='1.0', title='Bongo API', )
 app.register_blueprint(blueprint)
 
 # 분류 별 Prefix로 Namespace를 추가해주세요
-api.add_namespace(Login, '/member')			# 로그인
-api.add_namespace(Join, '/member')			# 회원가입
+api.add_namespace(Login, '/member')  # 로그인
+api.add_namespace(Join, '/member')  # 회원가입
+api.add_namespace(Search, '/search')		# 전체 검색
+api.add_namespace(Category, '/search')		# 카테고리 검색
+api.add_namespace(Work, '/work')		# 카테고리 검색
+api.add_namespace(Apply, '/work')		# 카테고리 검색
+api.add_namespace(Cancel, '/work')		# 카테고리 검색
+api.add_namespace(IDCheck, '/member')		# 아이디 중복확인
+api.add_namespace(GetProfile, '/member')    # 프로필 조회
 api.add_namespace(SetProfile, '/member')	# 프로필 등록
 api.add_namespace(SetInterest, '/member')	# 관심분야 등록
-api.add_namespace(IDCheck, '/member')		# 아이디 중복확인
-
-api.add_namespace(GetProfile, '/member')	# 프로필 조회
 """
 api.add_namespace(Business, '/business')	# 고용주 회원 관련
 api.add_namespace(Job, '/job')				# 업무 관련
