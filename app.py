3ea998672a6a837185fd2458b8ef46d9dd2c1eec
wishoon <heonga2@gmail.com>
2021-02-05 03:40:21 +0900

    분류 위치 수정

--- app.py
+++ app.py
@@ -14,15 +14,15 @@ app.register_blueprint(blueprint)
 # 분류 별 Prefix로 Namespace를 추가해주세요
 api.add_namespace(Login, '/member')  # 로그인
 api.add_namespace(Join, '/member')  # 회원가입
+api.add_namespace(IDCheck, '/member')		# 아이디 중복확인
+api.add_namespace(GetProfile, '/member')    # 프로필 조회
+api.add_namespace(SetProfile, '/member')	# 프로필 등록
+api.add_namespace(SetInterest, '/member')	# 관심분야 등록
 api.add_namespace(Search, '/search')		# 전체 검색
 api.add_namespace(Category, '/search')		# 카테고리 검색
 api.add_namespace(Work, '/work')		# 카테고리 검색
 api.add_namespace(Apply, '/work')		# 카테고리 검색
 api.add_namespace(Cancel, '/work')		# 카테고리 검색
-api.add_namespace(IDCheck, '/member')		# 아이디 중복확인
-api.add_namespace(GetProfile, '/member')    # 프로필 조회
-api.add_namespace(SetProfile, '/member')	# 프로필 등록
-api.add_namespace(SetInterest, '/member')	# 관심분야 등록
 """
 api.add_namespace(Business, '/business')	# 고용주 회원 관련
 api.add_namespace(Job, '/job')				# 업무 관련
@@ -31,4 +31,4 @@ api.add_namespace(Deposit, '/deposit')		# 예치금 관련
 """
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
