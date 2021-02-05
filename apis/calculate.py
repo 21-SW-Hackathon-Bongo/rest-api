d3068919c8b61717ce881f255feacfa4e6bf9576
wishoon <heonga2@gmail.com>
2021-02-06 04:52:14 +0900

    금일 남은 근무 시간 버그 수정

--- apis/calculate.py
+++ apis/calculate.py
@@ -12,12 +12,12 @@ sys.path.append("..")
 
 from lib.db_help import dbHelper
 
-# 검색 파라미터 정보
+
 Calculate = Namespace('calculate', description='일일 정산')
 calculate_model = Calculate.model('calculate data', {
     'user_seq': fields.Integer,
 })
-# 검색 API
+
 @Calculate.route('/')
 @Calculate.response(200, 'OK')
 @Calculate.response(500, 'Internal Error')
@@ -31,18 +31,19 @@ class Get_Calculate(Resource):
             parser.add_argument('user_seq', type=int)
             args = parser.parse_args()
 
-            sql = "SELECT TIMESTAMPDIFF(minute, now(), MIN(a.work_endAt)) AS deadline FROM bongo.work a, bongo.volunteer b WHERE a.work_seq = b.work_seq AND b.user_seq = %s ORDER BY a.work_endAt ASC";
+            sql = "SELECT TIMESTAMPDIFF(minute, now(), MAX(a.work_endAt)) AS deadline FROM bongo.work a, bongo.volunteer b WHERE a.work_seq = b.work_seq AND b.user_seq = %s ORDER BY a.work_endAt ASC";
 
             db.cursor.execute(sql, args['user_seq'])
             result = db.cursor.fetchall()
 
-            sql = "SELECT a.work_seq, a.work_title, a.work_pay, a.work_startAt, a.work_endAt FROM bongo.work a, bongo.volunteer b WHERE a.work_startAt < now() AND a.work_seq = b.work_seq AND b.user_seq = %s;"
-
-            db.cursor.execute(sql, args['user_seq'])
+            # JSON오류..
+            # sql = "SELECT a.work_seq, a.work_title, a.work_pay, a.work_startAt, a.work_endAt FROM bongo.work a, bongo.volunteer b WHERE a.work_startAt < now() AND a.work_seq = b.work_seq AND b.user_seq = %s;"
+            #
+            # db.cursor.execute(sql, args['user_seq'])
             result1 = db.cursor.fetchall()
 
-            for i in result1:
-                print(result1(i))
+            # for i in result1:
+            #     print(result1(i))
 
         except Exception as e:
             return {"code": "err", "message": str(e)}
