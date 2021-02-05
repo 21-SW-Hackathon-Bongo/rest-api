e329b9c11a3e0c1b22c895f94b98fe1522b0b545
wishoon <heonga2@gmail.com>
2021-02-05 23:33:02 +0900

    검색 Like문 버그 수정

--- apis/search.py
+++ apis/search.py
@@ -12,25 +12,21 @@ from lib.db_help import dbHelper
 Search = Namespace('search', description='일자리 이름 검색')
 
 
-# 현재 Like문 버그 있음 수정해야 함
+# 현재 Like문 버그?
 # 검색 API
-@Search.route('/<work_nm>')
+@Search.route('/<work_title>')
 @Search.response(200, 'OK')
 @Search.response(500, 'Internal Error')
 class Search_nm(Resource):
-    def get(self, work_nm):
+    def get(self, work_title):
         try:
             db = dbHelper()
+            MLN = "%s"% work_title
+            sql = "SELECT work_seq, work_title, work_intro, work_img, work_recruit, work_pay, work_deposit_yn, isDeleted FROM work a, work_type b, company c WHERE c.company_seq = a.company_seq AND a.work_type_seq = b.work_type_seq AND a.work_title LIKE '%s' AND a.work_deposit_yn = 'Y'" %(MLN);
 
-            sql = "SELECT * FROM work a, work_type b, company c WHERE c.company_seq = a.company_seq AND a.work_type_seq = b.work_type_seq AND a.work_title LIKE %s AND a.work_deposit_yn = 'Y'";
-
-            db.cursor.execute(sql, '%' + work_nm)
+            db.cursor.execute(sql)
             result = db.cursor.fetchall()
-
-            # # 추후 수정
-            # search_data = {"work_seq": result['work_seq'], "work_type_seq": result['work_type_seq'],
-            #                "company_seq": result['company_seq'], "work_title": result['work_title'], "work_pay": result['work_pay'],
-            #                "work_deposit_yn": result['work_deposit_yn'], "isDeleted": result['isDeleted']}
+            print(result)
 
         except Exception as e:
             return {"code": "err", "message": str(e)}
@@ -51,7 +47,7 @@ class Search_category(Resource):
         try:
             db = dbHelper()
 
-            sql = "SELECT * FROM work " \
+            sql = "SELECT work_seq, work_title, work_intro, work_img, work_recruit, work_pay, work_deposit_yn, isDeleted FROM work " \
                   "WHERE work_deposit_yn = %s " \
                   "AND work_type_seq = %s AND isDeleted = %s";
             db.cursor.execute(sql, ('Y', work_type_seq, 'N'))
@@ -61,3 +57,5 @@ class Search_category(Resource):
             return {"code": "err", "message": str(e)}
 
         return {"code": "success", "data": result}
+
+
