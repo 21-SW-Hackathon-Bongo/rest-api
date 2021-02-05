1d856b9842568f2c9f1e053fccb66cf130b359ad
wishoon <heonga2@gmail.com>
2021-02-05 16:37:52 +0900

    파라미터 모델 수정

--- apis/search.py
+++ apis/search.py
@@ -9,9 +9,10 @@ sys.path.append("..")
 from lib.db_help import dbHelper
 
 # 검색 파라미터 정보
-Search = Namespace('search', description='아이템 이름 검색')
+Search = Namespace('search', description='일자리 이름 검색')
 
 
+# 현재 Like문 버그 있음 수정해야 함
 # 검색 API
 @Search.route('/<work_nm>')
 @Search.response(200, 'OK')
@@ -26,6 +27,11 @@ class Search_nm(Resource):
             db.cursor.execute(sql, '%' + work_nm)
             result = db.cursor.fetchall()
 
+            # # 추후 수정
+            # search_data = {"work_seq": result['work_seq'], "work_type_seq": result['work_type_seq'],
+            #                "company_seq": result['company_seq'], "work_title": result['work_title'], "work_pay": result['work_pay'],
+            #                "work_deposit_yn": result['work_deposit_yn'], "isDeleted": result['isDeleted']}
+
         except Exception as e:
             return {"code": "err", "message": str(e)}
 
@@ -33,7 +39,7 @@ class Search_nm(Resource):
 
 
 # 아이템 카테고리 파라미터 정보
-Category = Namespace('category', description='카테고리를 통한 아이템 검색')
+Category = Namespace('category', description='일자리 카테고리 검색')
 
 
 # 아이템 카테고리 검색 API
@@ -45,8 +51,10 @@ class Search_category(Resource):
         try:
             db = dbHelper()
 
-            sql = "SELECT * FROM work a WHERE AND a.work_deposit_yn = 'Y' AND a.work_type_seq = %s AND a.isDeleted = %s";
-            db.cursor.execute(sql, work_type_seq, 'N')
+            sql = "SELECT * FROM work " \
+                  "WHERE work_deposit_yn = %s " \
+                  "AND work_type_seq = %s AND isDeleted = %s";
+            db.cursor.execute(sql, ('Y', work_type_seq, 'N'))
             result = db.cursor.fetchall()
 
         except Exception as e:
